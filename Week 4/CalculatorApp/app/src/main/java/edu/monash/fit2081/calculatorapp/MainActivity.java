d0f9bc441dc1709dc0fbee21cb1cd27fa1a0b63c
Alika Choo <alikachoo5@gmail.com>
2021-03-24 21:38:32 +0800

    Actually completed Task 1
    With functionality

--- Week 4/CalculatorApp/app/src/main/java/edu/monash/fit2081/calculatorapp/MainActivity.java
+++ Week 4/CalculatorApp/app/src/main/java/edu/monash/fit2081/calculatorapp/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.monash.fit2081.calculatorapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -35,7 +36,33 @@ public class MainActivity extends AppCompatActivity {
         decimalFormat = new DecimalFormat("#.##########");
     }
 
+    public void buttonZeroClick(View v) {
+        interScreen.setText(interScreen.getText() + "0");
+    }
 
+    public void buttonOneClick(View v) {
+        interScreen.setText(interScreen.getText() + "1");
+    }
+
+    public void buttonTwoClick(View v) {
+        interScreen.setText(interScreen.getText() + "2");
+    }
+
+    public void buttonThreeClick(View v) {
+        interScreen.setText(interScreen.getText() + "3");
+    }
+
+    public void buttonFourClick(View v) {
+        interScreen.setText(interScreen.getText() + "4");
+    }
+
+    public void buttonFiveClick(View v) {
+        interScreen.setText(interScreen.getText() + "5");
+    }
+
+    public void buttonSixClick(View v) {
+        interScreen.setText(interScreen.getText() + "6");
+    }
 
     public void buttonSevenClick(View v) {
         interScreen.setText(interScreen.getText() + "7");
@@ -47,6 +74,40 @@ public class MainActivity extends AppCompatActivity {
 
     public void buttonNineClick(View v) {
         interScreen.setText(interScreen.getText() + "9");
+    }
+
+
+    public void buttonAdditionClick(View v) {
+        computeCalculation();
+        if (Double.isNaN(valueOne)) {
+            showToast("Invalid Key");
+        } else {
+            CURRENT_ACTION = ADDITION;
+            resultScreen.setText(decimalFormat.format(valueOne) + "+");
+            interScreen.setText("");
+        }
+    }
+
+    public void buttonSubtractionClick(View v) {
+        computeCalculation();
+        if (Double.isNaN(valueOne)) {
+            showToast("Invalid Key");
+        } else {
+            CURRENT_ACTION = SUBTRACTION;
+            resultScreen.setText(decimalFormat.format(valueOne) + "-");
+            interScreen.setText("");
+        }
+    }
+
+    public void buttonMultiplicationClick(View v) {
+        computeCalculation();
+        if (Double.isNaN(valueOne)) {
+            showToast("Invalid Key");
+        } else {
+            CURRENT_ACTION = MULTIPLICATION;
+            resultScreen.setText(decimalFormat.format(valueOne) + "*");
+            interScreen.setText("");
+        }
     }
 
     public void buttonDivisionClick(View v) {
@@ -68,6 +129,67 @@ public class MainActivity extends AppCompatActivity {
         * Reset valueOne
         * Set CURRENT_ACTION to NO_OPERATION
         * */
+        computeCalculation();
+        String currInter = interScreen.getText().toString();
+        int i = 0;
+        if (currInter != "") {
+            i = Integer.parseInt(currInter);
+        }
+
+        String resStr = resultScreen.getText().toString();
+        int currRes = 0;
+        int result = i;
+        if (resStr != ""){
+            currRes = Integer.parseInt(resStr.substring(0, resStr.length()-1));
+
+            String operation = resStr.substring(resStr.length()-1);
+
+            switch(operation) {
+                case "+":
+                    CURRENT_ACTION = ADDITION;
+                    resultScreen.setText(decimalFormat.format(valueOne-valueTwo) + "+" + decimalFormat.format(valueTwo) + " = " + decimalFormat.format(valueOne));
+                    interScreen.setText("");
+                    break;
+                case "/":
+                    CURRENT_ACTION = DIVISION;
+                    resultScreen.setText(decimalFormat.format(valueOne*valueTwo) + "/" + decimalFormat.format(valueTwo) + " = " + decimalFormat.format(valueOne));
+                    interScreen.setText("");
+                    break;
+                case "*":
+                    CURRENT_ACTION = MULTIPLICATION;
+                    resultScreen.setText(decimalFormat.format(valueOne/valueTwo) + "*" + decimalFormat.format(valueTwo) + " = " + decimalFormat.format(valueOne));
+                    interScreen.setText("");
+                    break;
+                case "-":
+                    CURRENT_ACTION = SUBTRACTION;
+                    resultScreen.setText(decimalFormat.format(valueOne+valueTwo) + "+" + decimalFormat.format(valueTwo) + " = " + decimalFormat.format(valueOne));
+                    interScreen.setText("");
+                    break;
+                case "":
+                    result = i;
+                    break;
+            }
+        }
+
+//        String displayRes = resStr + currInter + " = " + String.valueOf(result);
+//        resultScreen.setText(displayRes);
+//        interScreen.setText("");
+    }
+
+    public void buttonDecimalClick(View v) {
+        String currInter = interScreen.getText().toString();
+        int i = 0;
+        if (currInter != "") {
+            i = Integer.parseInt(currInter);
+        }
+
+        String resStr = resultScreen.getText().toString();
+        String result = currInter;
+        if (currInter.substring(currInter.length() - 1) != ".") {
+            result = String.valueOf(currInter) + ".";
+        }
+        interScreen.setText(result);
+
     }
 
     public void buttonClearClick(View v) {
@@ -78,8 +200,11 @@ public class MainActivity extends AppCompatActivity {
               * reset valueOne, valueTwo, the content of result TextView,
               * and the content of intermediate TextView
         * */
+        valueOne = 0;
+        valueTwo = 0;
+        resultScreen.setText("");
+        interScreen.setText("");
     }
-
 
     private void computeCalculation() {
 

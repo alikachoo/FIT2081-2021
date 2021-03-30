6436a4006bb7c90a7c33cc23c741af373e021961
Alika Choo <alikachoo5@gmail.com>
2021-03-30 20:58:34 +0800

    Started ListView task

--- Week 5/CarsApp_Week5/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Week 5/CarsApp_Week5/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.Manifest;
 import android.content.BroadcastReceiver;
@@ -15,11 +18,16 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
@@ -46,13 +54,133 @@ public class MainActivity extends AppCompatActivity {
     EditText price;
     EditText address;
 
+    // Week 5 Tasks
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
+    // List cars
+    ListView listCars;
+    ArrayList<String> cars;
+    ArrayAdapter<String> adapter;
+
     Context self;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawer_layout);
+
+        cars = new ArrayList<>();
+
+        listCars = findViewById(R.id.listCars);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cars);
+        listCars.setAdapter(adapter);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView=findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.add_car:
+                            String res = "";
+                            String makerInput = maker.getText().toString();
+                            String modelInput = model.getText().toString();
+                            res = makerInput + "|" + modelInput;
+                            cars.add(res);
+                        break;
+                    case R.id.remove_last_car:
+
+                        break;
+                    case R.id.remove_all_cars:
+
+                        break;
+                }
+                return false;
+            }
+        });
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS, Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS}, 0);
+        MyBroadCastReceiver myBroadCastReceiver = new MyBroadCastReceiver();
+        registerReceiver(myBroadCastReceiver, new IntentFilter(SMSReceiver.SMS_FILTER));
+
+
+        maker = (EditText) findViewById(R.id.makerInput);
+        model = (EditText) findViewById(R.id.modelInput);
+        year = (EditText) findViewById(R.id.yearInput);
+        color = (EditText) findViewById(R.id.colorInput);
+        seats = (EditText) findViewById(R.id.seatsInput);
+        price = (EditText) findViewById(R.id.priceInput);
+        address = (EditText) findViewById(R.id.addressInput);
+
+        Log.d(WEEK_3_APP, "onCreate");
+
+        Context context = getApplicationContext();
+
+        maker = (EditText) findViewById(R.id.makerInput);
+// Previous workshop buttons
+//        button.setOnClickListener(new View.OnClickListener() {
+//            // Test
+//            /* Test Test*/
+//            @Override
+//            public void onClick(View v) {
+//                String makerInput = maker.getText().toString();
+//                String result = "We added a new car (" + makerInput + ")";
+//                Toast toast = Toast.makeText(context, result, Toast.LENGTH_SHORT);
+//                toast.show();
+//
+//                // Extra Lab Task Week 3 Question 2:
+//                SharedPreferences spMaker = getSharedPreferences(carMakerFile, 0);
+//                SharedPreferences.Editor ed = spMaker.edit();
+//                ed.putString(LOAD_MAKER_KEY,makerInput);
+//                ed.apply();
+//            }
+//        });
+//
+//        buttonReset.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                maker.setText("");
+//                model.setText("");
+//                year.setText("");
+//                color.setText("");
+//                seats.setText("");
+//                price.setText("");
+//                address.setText("");
+//
+//                Toast toast = Toast.makeText(self, "Reset successful!", Toast.LENGTH_SHORT);
+//                toast.show();
+//            }
+//        });
+//
+//        buttonLoad.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                SharedPreferences sP=getSharedPreferences(carMakerFile,0);
+//                String makerData = sP.getString(LOAD_MAKER_KEY, "");
+//                maker.setText(makerData);
+//                Toast toast = Toast.makeText(context, "Load successful", Toast.LENGTH_SHORT);
+//                toast.show();
+//            }
+//        });
+    }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        SharedPreferences sP=getPreferences(0);
+        SharedPreferences sP = getPreferences(0);
 
         String makerData = sP.getString(MAKER_KEY, "");
         String modelData = sP.getString(MODEL_KEY, "");
@@ -76,7 +204,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        SharedPreferences sP=getPreferences(0);
+        SharedPreferences sP = getPreferences(0);
         SharedPreferences.Editor editor = sP.edit();
         // Grab the data
         String makerData = maker.getText().toString();
@@ -154,77 +282,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.options_menu, menu);
         return true;
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.drawer_layout);
-
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS, Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS}, 0);
-        MyBroadCastReceiver myBroadCastReceiver = new MyBroadCastReceiver();
-        registerReceiver(myBroadCastReceiver, new IntentFilter(SMSReceiver.SMS_FILTER));
-
-
-        maker = (EditText) findViewById(R.id.makerInput);
-        model = (EditText) findViewById(R.id.modelInput);
-        year = (EditText) findViewById(R.id.yearInput);
-        color = (EditText) findViewById(R.id.colorInput);
-        seats = (EditText) findViewById(R.id.seatsInput);
-        price = (EditText) findViewById(R.id.priceInput);
-        address = (EditText) findViewById(R.id.addressInput);
-
-        Log.d(WEEK_3_APP, "onCreate");
-
-        Context context = getApplicationContext();
-
-        maker = (EditText) findViewById(R.id.makerInput);
-// Previous workshop buttons
-//        button.setOnClickListener(new View.OnClickListener() {
-//            // Test
-//            /* Test Test*/
-//            @Override
-//            public void onClick(View v) {
-//                String makerInput = maker.getText().toString();
-//                String result = "We added a new car (" + makerInput + ")";
-//                Toast toast = Toast.makeText(context, result, Toast.LENGTH_SHORT);
-//                toast.show();
-//
-//                // Extra Lab Task Week 3 Question 2:
-//                SharedPreferences spMaker = getSharedPreferences(carMakerFile, 0);
-//                SharedPreferences.Editor ed = spMaker.edit();
-//                ed.putString(LOAD_MAKER_KEY,makerInput);
-//                ed.apply();
-//            }
-//        });
-//
-//        buttonReset.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                maker.setText("");
-//                model.setText("");
-//                year.setText("");
-//                color.setText("");
-//                seats.setText("");
-//                price.setText("");
-//                address.setText("");
-//
-//                Toast toast = Toast.makeText(self, "Reset successful!", Toast.LENGTH_SHORT);
-//                toast.show();
-//            }
-//        });
-//
-//        buttonLoad.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                SharedPreferences sP=getSharedPreferences(carMakerFile,0);
-//                String makerData = sP.getString(LOAD_MAKER_KEY, "");
-//                maker.setText(makerData);
-//                Toast toast = Toast.makeText(context, "Load successful", Toast.LENGTH_SHORT);
-//                toast.show();
-//            }
-//        });
     }
 
     class MyBroadCastReceiver extends BroadcastReceiver {
